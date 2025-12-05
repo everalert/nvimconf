653b780f1efb477edec420761f1887afef1acaf5
Nathaniel <everalert@users.noreply.github.com>
2025-12-05 21:32:00 +1100

    fix: stop colorcolumn bg color overriding fg color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,15 +13,16 @@ return {
 			--undercurl = false,
 			commentStyle = { italic = false },
 			keywordStyle = { italic = false },
-			background = {
-				dark = "dragon",
-				light = "dragon",
-			},
+			--background = {
+			--	dark = "dragon",
+			--	light = "dragon",
+			--},
 			colors = {
 				theme = {
 					all = {
 						ui = {
-							bg_gutter = "none"
+							bg_gutter = "none",
+							--bg_p1 = "#1D1D24", -- ColorColumn BG
 						}
 					}
 				}
@@ -29,17 +30,36 @@ return {
 
 			overrides = function(colors)
 				local theme = colors.theme
+
 				local mix_color = function(color)
 					local c = require("kanagawa.lib.color")
 					return { fg = color, bg = c(color):blend(theme.ui.bg, 0.65):to_hex() }
 				end
+				
+				local mix_color_2b = function(fg_col, bg_col)
+					local c = require("kanagawa.lib.color")
+					return { fg = fg_col, bg = c(bg_col):blend(theme.ui.bg, 0.65):to_hex() }
+				end
+
+				local mix_color_2fb = function(fg_col, bg_col)
+					local c = require("kanagawa.lib.color")
+					return { 
+						fg = c(fg_col):blend(theme.ui.fg, 0.65):to_hex(),
+						bg = c(bg_col):blend(theme.ui.bg, 0.65):to_hex() 
+					}
+				end
+				
 				--local diag_color = function(color)
 				--	local c = require("kanagawa.lib.color")
 				--	return { bg = color, fg = "NONE" }
 				--end
 
 				return {
-					ColorColumn     = mix_color(theme.ui.bg_dim),
+					--ColorColumn     = mix_color(theme.ui.bg_dim),
+					ColorColumn     = mix_color_2b(colors.palette.oldWhite, theme.ui.bg_dim),
+					--ColorColumn     = mix_color_2fb(colors.palette.fujiWhite, theme.ui.bg_dim),
+					--ColorColumn     = mix_color_2fb(theme.ui.fg_dim, theme.ui.bg_dim),
+
 					--DiagnosticVirtualTextHint  = diag_color(theme.diag.hint),
 					--DiagnosticVirtualTextInfo  = diag_color(theme.diag.info),
 					--DiagnosticVirtualTextWarn  = diag_color(theme.diag.warning),
